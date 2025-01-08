176ac734c9eb3bf06ec0eb27837798e7ef4efedf
Alex Min <alexminth@gmail.com>
2025-01-08 11:24:08 -0500

    changed port

--- index.js
+++ index.js
@@ -79,6 +79,7 @@ app.delete('/deleteEntry', (req, res) => {//delete by date
         .catch(err => res.status(400).json('Error: ' + err));
 })
 
-app.listen(4000, () => {
-    console.log('Server is running on port 4000')
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
